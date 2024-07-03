5004530bb7b31aef77e745b2eff4df2d9d69a776
realistis <realistis@vocamen.com>
2024-07-03 15:54:30 +0200

    to fix pip install this fork

---
+++ django_baker/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+# Create your tests here.
+# Let's see about continuing our automation with
+# https://github.com/se2p/pynguin 
+# the PYthoN General UnIt test geNerator
+# :)
